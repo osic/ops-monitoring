144bc66b8bddabadd3a61bd1f2b2b20cba3744ee
Ala Raddaoui <raddaoui.ala@intel.com>
2016-10-10 18:58:36 +0000

    added filebeat to the data rotation

--- templates/rotate-topbeatdata.sh
+++ templates/rotate-topbeatdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for d in `find /var/lib/elasticsearch/openstack_elk/nodes/0/indices/ -maxdepth 1 -type d -mtime 5 | grep topbeat`;
+for d in `find /var/lib/elasticsearch/openstack_elk/nodes/0/indices/ -maxdepth 1 -type d -mtime +4 | grep -E 'topbeat|filebeat'`;
 do
   echo $d
   tar -zcvf $d.tar.gz $d && rm -r $d
